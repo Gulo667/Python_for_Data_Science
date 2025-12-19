6043c4a2f5091f18c818babc3f1e2a969fb06c88
Gulo <gulonamikaze@gmail.com>
2025-12-19 20:42:42 +0100

    adding new files

--- main.py
+++ main.py
@@ -25,10 +25,11 @@ print(df)
 #better to use jupiter btw
 #for this, you need to use Ctrl+Shift+p and write new jypiter notebook, or manually create a file with the extension .ipynb.
 #you can also use google colab for jupyter notebooks
-#you can also use anaconda for jupyter notebooks
-#you can also use vs code for jupyter notebooks
-#you can also use pycharm for jupyter notebooks
 #you need to install jupyter notebook using pip install jupyterlab or pip install notebook
 #you need to have python extension installed in vs code to use jypiter notebooks in vs code.
 #if you don't, you can go to extensions and search for python and install python extension.
+
+with open("C:/Users/user/Documents/Python for Data Science/text.txt", "r")as file1:
+    file_stuff=file1.read()
+    print(file_stuff)
     
